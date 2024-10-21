2d13a18e39e4a277db80fc196a5ab610baffde2b
Lorenzo-jaeger <lorenzobraga1@hotmail.com>
2024-10-21 14:38:08 -0300

    TryCatchThrow

---
+++ modulo-logica/TryCatchThrow/TryCatchThrow.js
@@ -0,0 +1,17 @@
+function soma(x, y) {
+    if (
+        typeof x !== 'number' ||
+        typeof y !== 'number'
+    ){
+        throw new Error('X e Y precisam ser numeros')
+    }
+    return x+y;
+}
+
+try {
+    console.log(soma(1,2));
+    console.log(soma('1',2));
+} catch (error) {
+    console.log(error);
+    
+}
